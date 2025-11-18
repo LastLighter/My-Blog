9dcb697ad28b4d70f5a3dd6a619060ab2dc6059b
LastLighter <2746115082@qq.com>
2025-11-18 14:47:35 +0800

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 export default defineConfig({
 	site: siteConfig.siteURL,
 
-	base: "/",
+	base: "/My-Blog/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
